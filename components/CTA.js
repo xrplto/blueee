c26da5293f774998a64cea55503329ddd9045ede
XRPL.to <93736363+xrplto@users.noreply.github.com>
2024-10-23 19:39:24 -0400

    deleted unused files

--- components/CTA.js
+++
@@ -1,9 +0,0 @@
-export default function CTA() {
-  return (
-    <section id="cta">
-      <h2>Join the BLUE Revolution</h2>
-      <p>Be part of the next big thing in the crypto world!</p>
-      <button id="blueButton" className="cta-button">Make Everything Blue</button>
-    </section>
-  )
-}
