94a4a9b31307de98575ee7bcfa0562b3a06b37c7
XRPL.to <93736363+xrplto@users.noreply.github.com>
2024-10-23 20:12:02 -0400

    update

--- blue-meme-coin/next.config.js
+++ blue-meme-coin/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  poweredByHeader: false,
+  generateEtags: false,
+  compress: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://xrpl.blue' : '',
+  trailingSlash: true,
+  experimental: {
+    optimizeCss: true,
+  },
+  serverRuntimeConfig: {
+    PORT: process.env.PORT || 3000
+  },
+  publicRuntimeConfig: {
+    staticFolder: '/static',
+  },
 }
 
 module.exports = nextConfig
